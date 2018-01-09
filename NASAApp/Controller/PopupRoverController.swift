6deda5bf6c199f3609239cc5d168856acd00fb1a
Angus Muller <angusmuller@anguss-mbp.lan>
2018-01-09 10:27:22 +0000

    Added rover popup controller for adding text dialog, delegate to add text to image and share button on as bar button

---
+++ NASAApp/Controller/PopupRoverController.swift
@@ -0,0 +1,45 @@
+//
+//  PopupRoverController.swift
+//  NASAApp
+//
+//  Created by Angus Muller on 09/01/2018.
+//  Copyright © 2018 Angus Muller. All rights reserved.
+//
+
+import UIKit
+
+protocol textEnteredDelegate: class {
+    func addTextToImage(text: String)
+}
+
+class PopupRoverController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var errorText: UILabel!
+    
+    weak var delegate: textEnteredDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+    @IBAction func addText(_ sender: Any) {
+        guard let text = textField.text, text != "" else {
+            //Show error message as of no text enetered
+            errorText.isHidden = false
+            return
+        }
+        
+        delegate?.addTextToImage(text: text)
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cancelPopup(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- NASAApp/Controller/RoverDetailController.swift
+++ NASAApp/Controller/RoverDetailController.swift
@@ -26,25 +26,41 @@ class RoverDetailController: UIViewController {
             DispatchQueue.global().async {
                 let data = try? Data(contentsOf: url)
                 DispatchQueue.main.async {
-                    self.roverPhotoView.image = UIImage(data: data!)
-                    let label = UILabel(frame: CGRect(x: 20, y: 20, width: 80, height: 30))
-                    label.text = "Rover on Mars"
-                    //label.font = UIFont.systemFont(ofSize: 20)
-                    //label.textColor = .black
-                    self.view.addSubview(label)
-                    self.overlayText(text: label)
-                    //self.roverPhotoView.clipsToBounds = true
-                    //self.view.addSubview(self.roverPhotoView)
+                    self.roverPhotoView.image = UIImage(data: data!) //NOTE: Checking image data there or bringing up error
                     
-                    self.shareImage()
                 }
             }
         }
     }
     
-    func overlayText(text: UILabel) {
-        roverPhotoView.drawOnCurrentImage(view: text, mode: .changeOriginalImage) { (parentSize, viewToAdd) in
-            print("ParentSize: \(parentSize)")
+    
+    @IBAction func shareImage(_ sender: Any) {
+        let activityViewController = UIActivityViewController(activityItems: [roverPhotoView.image], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "roverPopup") {
+            if let popUpViewController = segue.destination as? PopupRoverController {
+            popUpViewController.delegate = self
+            }
+        }
+    }
+}
+
+extension RoverDetailController: textEnteredDelegate {
+    func addTextToImage(text: String) {
+        
+        let label = UILabel(frame: CGRect(x: 20, y: 20, width: 80, height: 30))
+        label.text = text
+        self.view.addSubview(label)
+        
+        roverPhotoView.drawOnCurrentImage(view: label, mode: .changeOriginalImage) { (parentSize, viewToAdd) in
             viewToAdd.font = UIFont.systemFont(ofSize: 40)
             viewToAdd.textColor = .yellow
             viewToAdd.textAlignment = .center
@@ -52,18 +68,8 @@ class RoverDetailController: UIViewController {
             let height: CGFloat = 30
             let indent: CGFloat = 40
             viewToAdd.bounds = CGRect(x: 0, y: parentSize.height - height - indent, width: parentSize.width, height: height)
-            
-            
         }
     }
     
-    func shareImage() {
-        let activityViewController = UIActivityViewController(activityItems: [roverPhotoView.image], applicationActivities: nil)
-        activityViewController.popoverPresentationController?.sourceView = self.view
-        
-        self.present(activityViewController, animated: true, completion: nil)
-    }
-
-  
-
+    
 }
